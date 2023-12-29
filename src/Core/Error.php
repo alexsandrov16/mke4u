ddec944fd27bba38cc81e6a6364384ff8015acd8
alexsandrov16 <al3x5.dev@gmail.com>
2023-12-29 11:03:53 -0500

    Add HttpException

--- src/Core/Error.php
+++ src/Core/Error.php
@@ -2,7 +2,8 @@
 
 namespace Mk4U\Core;
 
-use Mk4U\Http\Response;
+use Mk4U\Http\Exceptions\HttpExceptions;
+use Mk4U\Http\HttpStatus;
 use Whoops\Handler\Handler;
 
 /**
@@ -16,29 +17,13 @@ use Whoops\Handler\Handler;
 class Error extends Handler
 {
     /**
-     * Devuelve
+     * Devuelve plantilla de error personalizada
      */
     public function handle(): ?int
     {
-        echo Response::html($this->html(), 500);
-        return Handler::QUIT;
-    }
+        $e = new HttpExceptions(HttpStatus::InternalServerError);
+        echo $e->render();
 
-    /**
-     * Plantilla de error 500 predeterminada
-     */
-    public function html(): string
-    {
-        return <<<HTML
-        <html>
-        <head>
-            <title>Server Error</title>
-            <style>*{margin:0;padding:0;}body{height:80vh;display:flex;justify-content:center;align-items:center;font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;color: #535353;background-color: #e7e7e7af;}h1{font-size:3.25em;font-weight:400;}</style>
-        </head>
-        <body>
-            <h1>Error 500</h1>
-        </body>
-        </html>
-        HTML;
+        return Handler::QUIT;
     }
 }
